44554940bd4a54a6dd8693de378251b9c1d8e0aa
Sayemahamed <sayemahamed183@gmail.com>
2025-11-12 20:55:59 +0600

    retry

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from backend!")
-
-
-if __name__ == "__main__":
-    main()
+from fastembed import TextEmbedding
+documents = [
+    "FastEmbed is lighter than Transformers & Sentence-Transformers.",
+    "FastEmbed is supported by and maintained by Qdrant.",
+]
+embedding_model = TextEmbedding()
+embeddings_generator = embedding_model.embed(documents)
+embeddings_list = list(embeddings_generator)
+print(embeddings_list)
